96e8241a87f2f64240807d0d6e6de7557bd4d0be
/cguzman <cguzman@expressingenieria.com>
2020-10-09 11:51:27 -0500

    First Deploy App Front Example

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const axios = require('axios').default
 
 axios.defaults.withCredentials = true;
+axios.defaults.credentials= 'include';
 
 async function getToken(product) {
   try {
@@ -10,18 +11,20 @@ async function getToken(product) {
           // url: `http://localhost:8080/api/auth/sign-in`,
           timeout: 3000,
           data: {
-            "apiKeyToken":"e335916b9ca9647b1cf3365a94817d9f93eedd6b2a5a87741b5069c12aa985b5"
+            // "apiKeyToken":"e335916b9ca9647b1cf3365a94817d9f93eedd6b2a5a87741b5069c12aa985b5", // AVALON LOCAL
+            "apiKeyToken":"98fbbd37e1daf634fd7b322d8d5de856f1301da90d4c29ed28dd96299a8b449f" // AVALON REMOTE
           },
           headers: {
             "Content-Type": "application/json",
-            // 'X-Requested-With': 'XMLHttpRequest',
-            // 'Access-Control-Allow-Origin': '*',  
+            'X-Requested-With': 'XMLHttpRequest',
+            'Access-Control-Allow-Origin': '*',  
           },
           auth: {
             username: 'cguzman@expressingenieria.com',
             password: 'ing870915' // Bad password
           },
       });
+      console.log("DATA-------->");
       console.log(data);
       return data;
   } catch (error) {
